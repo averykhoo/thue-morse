c013eeef1416d98c9cdf15ced7c749d127ab7587
averykhoo <averykhoo@gmail.com>
2020-10-23 09:38:09 +0800

    add thue-morse by n-ary expansion

---
+++ n_ary_expansion.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+from thue_morse import generate
+
+
+def n_ary(number, n):
+    nums = []
+    while number:
+        number, r = divmod(number, n)
+        nums.append(r)
+    return nums[::-1]
+
+
+def thue_morse_idx(index, dim):
+    return sum(n_ary(index, dim)) % dim
+
+
+if __name__ == '__main__':
+
+    tm = []
+    c = Counter()
+    for i in range(1235):
+        tmi = thue_morse_idx(i, 5)
+        tm.append(tmi)
+        c[tmi] += 1
+
+    print(c)
+    print(tm[:1000])
+
+    tm2 = generate(5, 1000, trim=True)
+    print(tm2)
+    print(tm[:1000] == tm2)
