403040004d74de3262bb1d17eb4ed3ad890572be
averykhoo <averykhoo@gmail.com>
2020-10-23 10:15:44 +0800

    add popocount

--- n_ary_expansion.py
+++ n_ary_expansion.py
@@ -7,6 +7,13 @@ from collections import Counter
 from thue_morse import generate
 
 
+def popcount64(number):
+    assert 0 <= number <= 0xFFFFFFFFFFFFFFFF  # max unsigned 64-bit
+    number -= ((number >> 1) & 0x5555555555555555)
+    number = (number & 0x3333333333333333) + (number >> 2 & 0x3333333333333333)
+    return (((number + (number >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101) & 0xFFFFFFFFFFFFFFFF >> 56
+
+
 def n_ary(number, n):
     nums = []
     while number:
@@ -16,7 +23,10 @@ def n_ary(number, n):
 
 
 def thue_morse_idx(index, dim):
-    return sum(n_ary(index, dim)) % dim
+    if dim == 2:
+        return popcount64(index)
+    else:
+        return sum(n_ary(index, dim)) % dim
 
 
 if __name__ == '__main__':
