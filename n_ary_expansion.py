eae835c932843dc3bab90ccdcedaa1588f785d6f
averykhoo <averykhoo@gmail.com>
2020-10-23 10:35:37 +0800

    so the n-ary expansion is slow, albeit interesting

--- n_ary_expansion.py
+++ n_ary_expansion.py
@@ -1,9 +1,11 @@
 """
 sum-of-terms definition (§2.1)
 in base-2, basically just popcount
+TLDR: it works and it's correct but it's also slow so dont use it
 """
-from collections import Counter
+import itertools
 
+from thue_morse import fair_test
 from thue_morse import generate
 
 
@@ -29,18 +31,41 @@ def thue_morse_idx(index, dim):
         return sum(n_ary(index, dim)) % dim
 
 
+def thue_morse_iter(num_players, min_length, trim=False):
+    seq = list(range(num_players))
+    seqs = []
+    i = 0
+    while len(seqs) < min_length:
+        i += 1
+        seqs = list(itertools.product(seq, repeat=i))
+
+    out = [sum(seq) % num_players for seq in seqs]
+    return out[:min_length] if trim else out
+
+
 if __name__ == '__main__':
+    n = 2
+    min_len = 99999
 
-    tm = []
-    c = Counter()
-    for i in range(1235):
-        tmi = thue_morse_idx(i, 5)
-        tm.append(tmi)
-        c[tmi] += 1
 
-    print(c)
-    print(tm[:1000])
+    def f1():
+        return generate(n, min_len)
 
-    tm2 = generate(5, 1000, trim=True)
-    print(tm2)
-    print(tm[:1000] == tm2)
+
+    def f2():
+        return [thue_morse_idx(i, n) for i in range(min_len)]
+
+
+    def f3():
+        return thue_morse_iter(n, min_len)
+
+
+    print(len(f1()))
+    print(len(f2()))  # slow
+    print(len(f3()))  # super slow
+
+    print(list(fair_test(f1, f2, f3, min_len=10, n_iterations=10)))
+    # [('f1', 2.8893837928771973),
+    #  ('f2', 18.235366582870483),
+    #  ('f3', 20.77123260498047),
+    # ]
